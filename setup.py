f772d3ca77f5caff6149b806420b99584a92b779
aibi10 <isingh.abhishek10@gmail,com>
2022-09-02 04:28:49 +0530

    updated readme

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-## edit below variables as per your requirements -
+# edit below variables as per your requirements -
 REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "aibi10"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
 
